844cbca7006eeb5e287061384d6fbf2bf16499a3
Chris <christian.bang@gmail.com>
2025-05-09 07:44:14 +0200

    fix darkmode

--- src/routes/[[playlistId]]/Streams.svelte
+++ src/routes/[[playlistId]]/Streams.svelte
@@ -5,7 +5,9 @@
 <div class="flex-1 flex flex-col gap-2 row-span-2 overflow-auto">
 	{#each videos as video}
 		<button
-			class="{activeStream == video ? 'bg-lime-400' : ''} cursor-pointer text-left flex-col flex"
+			class="{activeStream == video
+				? 'bg-lime-400 dark:bg-gray-700'
+				: ''} cursor-pointer text-left flex-col flex"
 			onclick={() => {
 				playSong(video);
 			}}
