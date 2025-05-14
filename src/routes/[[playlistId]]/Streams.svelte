017aa945850c0f6eabcf70deafd51654ef21e22e
Chris <christian.bang@gmail.com>
2025-05-14 16:32:26 +0200

    clearer buttons

--- src/routes/[[playlistId]]/Streams.svelte
+++ src/routes/[[playlistId]]/Streams.svelte
@@ -7,19 +7,15 @@
 		<button
 			class="{activeStream == video
 				? 'bg-lime-400 dark:bg-gray-700'
-				: ''} cursor-pointer text-left flex-col flex rounded-2xl"
+				: 'bg-zinc-100 dark:bg-gray-400'} cursor-pointer text-left flex-col flex rounded-2xl"
 			onclick={() => {
 				console.log(video);
 
 				playSong(video);
 			}}
 		>
-			<div class="stream flex flex-row gap-4">
-				<img
-					class="size-[100px] rounded object-cover"
-					src={video.snippet?.thumbnails.medium.url}
-					alt=""
-				/>
+			<div class="stream rounded-xl overflow-hidden flex flex-row gap-4">
+				<img class="size-[100px] object-cover" src={video.snippet?.thumbnails.medium.url} alt="" />
 				<div class="flex flex-col justify-center">
 					<div class="text-lg lg:text-xl">{video?.snippet?.title}</div>
 					<small class="text-neutral-600 dark:text-neutral-300">
