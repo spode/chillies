39e6cc2c0a74f5d528cee65b6db7366a0de2f174
Chris <christian.bang@gmail.com>
2025-05-14 16:34:34 +0200

    use html-entities

--- src/routes/[[playlistId]]/Streams.svelte
+++ src/routes/[[playlistId]]/Streams.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { decode } from 'html-entities';
 	let { videos, playSong, activeStream } = $props();
 </script>
 
@@ -17,7 +18,7 @@
 			<div class="stream rounded-xl overflow-hidden flex flex-row gap-4">
 				<img class="size-[100px] object-cover" src={video.snippet?.thumbnails.medium.url} alt="" />
 				<div class="flex flex-col justify-center">
-					<div class="text-lg lg:text-xl">{video?.snippet?.title}</div>
+					<div class="text-lg lg:text-xl">{decode(video?.snippet?.title)}</div>
 					<small class="text-neutral-600 dark:text-neutral-300">
 						{video?.snippet?.publishedAt &&
 							new Date(video?.snippet?.publishedAt).toLocaleDateString()}
